8cca5e0567d9e65e5b0be9038b8d372ed9d91e6e
Ritesh <riteshrajputt21@gmail.com>
2026-01-10 02:29:02 +0530

    Add explicit component scan

--- src/main/java/com/example/Chat_Application/ChatApplication.java
+++ src/main/java/com/example/Chat_Application/ChatApplication.java
@@ -2,8 +2,10 @@ package com.example.Chat_Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.Chat_Application")  // ✅ Explicit scan
 public class ChatApplication {
 
 	public static void main(String[] args) {
